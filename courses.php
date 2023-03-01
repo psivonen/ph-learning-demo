f3716082dc786c0c2a7aba91491b4b79758b7188
psivonen <petra.sivonen@gmail.com>
2023-03-01 18:50:09 +0200

    courses page

---
+++ courses.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$title = "Courses & Workshops";
+include('header2.php'); 
+?>
+
+<div class="course-hero">
+        <div class="course-hero-content">
+            <h1>Find your course or workshop and improve your photography skills</h1>
+            <p class="lead">Nam tempus tellus non nibh dictum facilisis. 
+                Nam urna massa, posuere eu mauris sed, condimentum imperdiet turpis. 
+                Duis mollis libero sed dapibus efficitur. 
+                Etiam maximus, nulla quis euismod pharetra, nulla purus condimentum lacus, eu sagittis sem lorem non erat. </p>
+        </div>
+    </div>
+
+<div class="container-lg">
+    <div class="container-md">
+        <div class="column">
+            <h2>Courses for different levels</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula, mi at rhoncus fringilla, 
+                dolor nibh suscipit risus, vel porta enim mauris gravida metus. Nulla facilisi. 
+                Vestibulum fermentum dictum dolor, ac suscipit ligula molestie vel.
+                <br>
+                <br>
+                Mauris quam nibh, consequat ut tristique in, laoreet vel est. 
+                Praesent mi orci, consectetur nec commodo ut, viverra eget nisi. 
+                Sed aliquam mauris a eros porta faucibus. Duis eu ornare nunc. 
+            </p>
+        </div>
+        <div class="column">
+            <img src="Images/person-with-camera.jpg" alt="person with a camera on top of mountain" width="100%">
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Upcoming courses and workshops</h2>
+        <div class="container-sm">
+            <div class="gallery">
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+                <div class="gallery-item">
+                    <p class="uppercase">Beginner / Price 120€</p>
+                    <div>
+                        <h4>Course / workshop name</h4>
+                        <p>Pellentesque accumsan tincidunt ipsum, et consectetur elit egestas vel. Quisque in dapibus mauris, non pellentesque lorem. </p>
+                        <button class="btn btn-outline-light">Read more</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contact">
+    <div class="content">
+        <h3>Have any questions about the courses or workshops?</h3>
+        <button class="btn btn-outline-dark">Send a message</button>
+    </div>
+ </div>
+
+<?php include('footer.php'); ?>
+
+</html>
---
+++ header2.php
@@ -0,0 +1,36 @@
+<?php 
+
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'Valokuvaus koulutus';?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="scripts.js" async defer></script>
+</head>
+
+<body>
+
+    <div class="navigation" id="nav-bar">
+        <div class="logo">Company.</div>
+        <a href="javascript:void(0);" class="nav-icon" onclick="navToggle()">
+            <i class="fa fa-bars"></i>
+        </a>
+        <div class="nav-links" id="topNav">
+            <a href="index.php" class="<?= ($activePage == 'index') ? 'active':'nav-link'; ?>">Home</a>
+            <a href="" class="<?= ($activePage == 'photography') ? 'active':'nav-link'; ?>">Photography</a>
+            <a href="" class="<?= ($activePage == 'courses') ? 'active':'nav-link'; ?>">Courses & Workshops</a>
+            <a href="" class="<?= ($activePage == 'portfolio') ? 'active':'nav-link'; ?>">Portfolio</a>
+            <a href="" class="<?= ($activePage == 'about') ? 'active':'nav-link'; ?>">About</a>
+            <a href="" class="<?= ($activePage == 'contact') ? 'active':'nav-link'; ?>">Contact</a>
+        </div>
+    </div>
